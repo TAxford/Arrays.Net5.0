0a724f3ded84eb1e55e145257acf95f7b0f25357
Timothy Axford <t.axford2@yahoo.com>
2023-03-19 21:15:36 -0400

    Created a Console to call out and display the grade at index 0

--- Arrays.Net5.0/Program.cs
+++ Arrays.Net5.0/Program.cs
@@ -10,9 +10,12 @@ namespace Arrays.Net5._0
 
             grades[0] = 20;
             grades[1] = 15;
-            grades[2] = 15;
-            grades[3] = 15;
-            grades[4] = 15;
+            grades[2] = 10;
+            grades[3] = 5;
+            grades[4] = 0;
+
+            Console.WriteLine("grades at index 0 : {0}", grades[0]);
+            Console.ReadKey();
         }
     }
 }
