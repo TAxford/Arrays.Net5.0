f5df16b622367c70e18326baf261b48e24ca0b80
Timothy Axford <t.axford2@yahoo.com>
2023-03-19 21:13:16 -0400

    Added Grade values to each array index

--- Arrays.Net5.0/Program.cs
+++ Arrays.Net5.0/Program.cs
@@ -7,6 +7,12 @@ namespace Arrays.Net5._0
         static void Main(string[] args)
         {
             int[] grades = new int[5];
+
+            grades[0] = 20;
+            grades[1] = 15;
+            grades[2] = 15;
+            grades[3] = 15;
+            grades[4] = 15;
         }
     }
 }
