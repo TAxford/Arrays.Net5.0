7ed049769374cd46e8005f9d03fa9fbf47c25d7d
Timothy Axford <t.axford2@yahoo.com>
2023-03-19 21:22:17 -0400

    Added another example of how to initialize an array

--- Arrays.Net5.0/Program.cs
+++ Arrays.Net5.0/Program.cs
@@ -23,6 +23,9 @@ namespace Arrays.Net5._0
             Console.WriteLine("grades at index 0 : {0}", grades[0]);
             Console.ReadKey();
              */
+
+            //Another way of initializing an array
+            int[] gradesOfMathStudentsA = { 20, 13, 12, 8, 8 };
         }
     }
 }
